cbb310ae491c70cf78faa5030eea86bf6501b325
faldymartinus <faldymartinus@gmail.com>
2022-05-29 17:08:09 +0700

    add users pages

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const mongo = require('mongoose')
 const bp = require('body-parser')
 const landingRoutes = require('./routes/landingRoutes')
 const authData = require('./routes/authRoutes')
+const editDataForm = require('./routes/editDataRoutes')
+const editUserForm = require('./routes/editUserRoutes')
+const dataTable = require('./routes/dataTableRoutes')
+const userTable = require('./routes/userTableRoutes')
 const {connectDb} = require('./mongo/mongo-connection')
 
 require('dotenv').config()
@@ -21,10 +25,16 @@ app.set('view engine','ejs')
 
 app.use('/',landingRoutes)
 app.use('/auth',authData)
+app.use('/editTempatParkir',editDataForm)
+app.use('/editUser',editUserForm)
+app.use('/tempatParkir',dataTable)
+app.use('/user',userTable)
+
 
 
 app.listen(5000,()=>{
     console.log('listen at 5000')
 })
+
 
 
